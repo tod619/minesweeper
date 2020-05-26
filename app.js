64f76636330915dd8bac4bf13a6c8578d86cf9b5
Trevor O'Dwyer <trevorodwyer619@hotmail.com>
2020-05-26 10:16:56 +0100

    Add bomb class to divs using app.js + style.css

--- app.js
+++ app.js
@@ -11,13 +11,14 @@ function createBoard() {
     const emptyArray = Array(width * width - bombAmount).fill('valid');
     const gameArray = emptyArray.concat(bombsArray);
     const shuffledArray = gameArray.sort(() => Math.random() - 0.5);
-    console.log(shuffledArray);
+
 
 
 
     for (let i = 0; i < width * width; i++) {
         const square = document.createElement('div');
         square.setAttribute('id', i);
+        square.classList.add(shuffledArray[i]);
         grid.appendChild(square);
         squares.push(square);
     }
