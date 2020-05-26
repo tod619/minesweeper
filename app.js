6e7ec32af7d785b0e7a7a6b99630f6e0f5694c4f
Trevor O'Dwyer <trevorodwyer619@hotmail.com>
2020-05-26 10:12:49 +0100

    Create a shuffled array to hold game pieces in app.js file

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const grid = document.querySelector('.grid');
 let width = 10;
+let bombAmount = 20;
 let squares = [];
 
 // Create Game Board
 function createBoard() {
+
+    //get shuffled game array with random bombs
+    const bombsArray = Array(bombAmount).fill('bomb');
+    const emptyArray = Array(width * width - bombAmount).fill('valid');
+    const gameArray = emptyArray.concat(bombsArray);
+    const shuffledArray = gameArray.sort(() => Math.random() - 0.5);
+    console.log(shuffledArray);
+
+
+
     for (let i = 0; i < width * width; i++) {
         const square = document.createElement('div');
         square.setAttribute('id', i);
